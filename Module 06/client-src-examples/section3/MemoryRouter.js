7fd3f25fe1672a5a5058e304f2c0b365b4132671
Sam Chung <chungsam@cityu.edu>
2024-10-23 07:18:41 -0700

    Update MemoryRouter.js

--- Module 06/client-src-examples/section3/MemoryRouter.js
+++ Module 06/client-src-examples/section3/MemoryRouter.js
@@ -6,7 +6,6 @@ import { MemoryRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Details from './Details';
-import './App.css';
 
 class App extends Component {
   render() {
